eb353148de2ff64ce7f6cc0f2ff7797c28d55124
JohnGordon1 <john@estateslawncare.com>
2023-05-23 19:17:19 -0400

    Created an emoji.

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -4,4 +4,24 @@ function setup() {
 
 function draw() {
   background(220);
+
+  fill("yellow");
+  ellipse(200,200,250,250);
+
+fill("black");
+ellipse(150,150,25,75);
+ellipse(250,150,25,75);
+ellipse(200,220,60,60);
+
+
+
 }
+  
+
+
+
+
+
+
+
+
