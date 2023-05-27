c9b46885e70051206d48c7b32cc0139b0210cc88
JohnGordon1 <john@estateslawncare.com>
2023-05-27 16:05:48 -0400

    Emulation sketch

--- week2/2.3.emulation/sketch.js
+++ week2/2.3.emulation/sketch.js
@@ -1,7 +1,35 @@
 function setup() {
   createCanvas(400, 400);
+  background(255);
+  noLoop();
 }
 
 function draw() {
-  background(255);
+  const squareSize = 20;
+  const spacing = 5;
+  const rows = 14;
+  const cols = 14;
+  
+  const startX = (width - (cols * squareSize + (cols - 1) * spacing)) / 2;
+  const startY = (height - (rows * squareSize + (rows - 1) * spacing)) / 2;
+  
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
+      const xPos = startX + (x * (squareSize + spacing));
+      const yPos = startY + (y * (squareSize + spacing));
+      
+      stroke(0);
+      strokeWeight(1);
+      noFill();
+      
+      if (x % 2 === 0 && y % 2 === 0) {
+        rect(xPos, yPos, squareSize, squareSize);
+      } else if (x % 2 === 1 && y % 2 === 1) {
+        fill(0);
+        rect(xPos, yPos, squareSize, squareSize);
+      }
+    }
+  }
 }
+
+
