5beca0d2cde5f0c55c04cac995c382bd55d45f0e
JohnGordon1 <john@estateslawncare.com>
2023-05-31 09:27:56 -0400

    Bounce Final

--- week3/3.1.bounce/sketch.js
+++ week3/3.1.bounce/sketch.js
@@ -1,7 +1,37 @@
+// Variables
+ballX = 200;
+ballY = 50;
+ballDX = 6;
+ballDY = 3;
+GRAVITY = 0.1;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 300);
 }
 
 function draw() {
-  background(220);
+  background("purple");
+
+// Move the ball
+  ballDY += GRAVITY;
+  ballY += ballDY;
+  ballX += ballDX;
+
+  // Bounce
+  if(ballY>290) {
+    ballDY *= -1;
+  }
+
+  if(ballX>390) {
+    ballDX *= -1;
+  }
+
+  if(ballX<10) {
+    ballDX *= -1;
+  }
+
+  // Draw ball
+  fill("turquoise");
+  circle(ballX, ballY, 55);
+
 }
