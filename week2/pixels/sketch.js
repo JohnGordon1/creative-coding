a1dc71343415ba1dba54348df32beec55d88d579
JohnGordon1 <john@estateslawncare.com>
2023-05-31 22:49:05 -0400

    Pixel Project Final

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,7 +1,35 @@
+
+let img;
+let sampleX = 170;
+let sampleY = 170;
+
+function preload(){
+  img = loadImage('https://johngordon1.github.io/creative-coding/week2/pixels/flower_image.jpg');
+}
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(900, 900);
+  img.loadPixels();
 }
 
 function draw() {
   background(220);
+  noStroke();
+  translate(1, 1);
+  //image(img, 0, 0);
+
+  for (let x = 0; x <= img.width; x += sampleX){
+  for (let y = 0; y <= img.height; y += sampleY){
+    var index = (x + y * img.width) * 4;
+    var r = img.pixels[index + 0]; 
+    var g = img.pixels[index + 1]; 
+    var b = img.pixels[index + 2]; 
+  
+    fill(r,g,b);
+    circle(x,y,sampleX,sampleY);
+
+  }
+ }
+
+
 }
