57640ff804ce78606adda7ad1a4d48513c80f252
JohnGordon1 <john@estateslawncare.com>
2023-06-10 19:53:38 -0400

    Markov Letter Final

--- week4/4.3.love-markov/sketch.js
+++ week4/4.3.love-markov/sketch.js
@@ -1,7 +1,46 @@
+
+//create markov model
+let rm= RiTa.markov(2);
+
+//find some data
+let data = document.getElementById("source").innerText;
+
+//train the model
+rm.addText(data);
+
+
+
 function setup() {
-  createCanvas(800, 800);
+
+ //generate some sentences
+  let lines = rm.generate(6);
+
+//find the letter DIV
+let letter = select("#letter");
+
+//add salutation
+letter.child(  createP("Dear Catherine,") );
+
+for (let l = 0; l < lines.length; l++){
+  letter.child(
+      createP( lines[l] )
+    );
 }
 
-function draw() {
-  background(220);
+// add closing
+letter.child( createP("Yours Truly,<br>Andrey Markov") );
 }
+
+function mousePressed(){
+  letter.remove();
+  letter = createDiv();
+  letter.id("letter");
+  setup();
+
+
+}
+
+
+
+
+
