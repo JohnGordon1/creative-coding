dd783e7624e6719868cce69e008cb5dee12b5995
JohnGordon1 <john@estateslawncare.com>
2023-06-13 14:27:53 -0400

    Book Final

--- week4/book/sketch.js
+++ week4/book/sketch.js
@@ -1,3 +1,28 @@
-function setup() {
+let myBook = "";
 
+function setup() {
+	noCanvas();
+
+	let content = createDiv()
+	content.id("content");
+
+	let bookTitle = createElement("h1","The Love of My Life");
+
+	content.child(bookTitle);
+
+	for (let w = 0; w < 50000; w++){
+		myBook += random(["Alexandria ", "aLeXANdria ", "ALEXANDRIA ", "ALEXandriA "]);
+		myBook += random(["", "", "", "", "",".",]);
+
+		
+	}
+
+	let bookContent = createDiv(myBook);
+
+	content.child(bookContent);
+	
+	Bindery.makeBook({
+		content: '#content',
+	})
 }
+
